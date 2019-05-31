35bb0d1adeb63ded1f7d0c7852f16057433a51c4
skamipsi <50922796+skamipsi@users.noreply.github.com>
2019-05-31 13:13:24 +0300

    Create home4.3.php

---
+++ home4.3.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$dni = [1=>"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+$dayNumber = date('N');
+
+foreach ($dni as $key=>$element){
+    if($key == $dayNumber) {
+        print "<i>$element</i><br>";
+        $day = $element;
+    }
+    }
+    print "$element<br>";
+};
+echo  $day;
