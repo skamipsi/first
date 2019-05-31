90c5b00c7359d3276011cfb29ebd7de7d3a344b0
skamipsi <50922796+skamipsi@users.noreply.github.com>
2019-05-31 12:02:43 +0300

    Update home4.1.php

--- home4.1.php
+++ home4.1.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Home 4.1</title>
-
-</head>
-<body>
-
 <?php
-$mass = [1 2 3 4 5];
+$mass = [1, 2, 3, 4, 5];
 $result = 0;
 foreach ($mass as $element)
 {
@@ -16,7 +7,3 @@ foreach ($mass as $element)
 
 }
 echo "Сумма квадратов элементов массива =".$result;
-var_dump($result);
-?>
-
-</body>
