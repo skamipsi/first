44396555deae203780977f7a909f377f4b79d509
skamipsi <50922796+skamipsi@users.noreply.github.com>
2019-06-03 17:36:47 +0300

    Update home4.1.php

--- home4.1.php
+++ home4.1.php
@@ -1,9 +1,7 @@
 <?php
 $mass = [1, 2, 3, 4, 5];
 $result = 0;
-foreach ($mass as $element)
-{
+foreach ($mass as $element){
     $result += $element*$element;
-
 }
 echo "Сумма квадратов элементов массива = ".$result;
