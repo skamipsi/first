2a7ae8d1d5982c63f7715830ae0bf7d3ad92c8f2
Sergey Skami <skami@ukr.net>
2019-06-03 21:02:42 +0300

    lesson4

---
+++ form.php
@@ -0,0 +1,55 @@
+<?php
+
+$result = '';
+$operand2 = '';
+$operand1 = '';
+$operator = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $parameters = ['operand1', 'operator', 'operand2'];
+
+    foreach ($parameters as $parameter) {
+        if (empty($_POST[$parameter])) {
+            die('Не найден элемент ' . $parameter);
+        }
+    }
+
+    /*
+    if(empty($_POST['operand1'])){
+        die('Не найден operand1');
+    }
+    if(empty($_POST['operator'])){
+        die('Не найден operator');
+    }
+
+    if(empty($_POST['operand2'])){
+        die('Не найден operand2');
+    }
+    */
+
+    $operand1 = floatval($_POST['operand1']);
+    $operator = $_POST['operator'];
+    $operand2 = floatval($_POST['operand2']);
+
+    switch ($operator) {
+        case '+':
+            $result = $operand1 + $operand2;
+            break;
+        case '-':
+            $result = $operand1 - $operand2;
+            break;
+        case '*':
+            $result = $operand1 * $operand2;
+            break;
+        case '/':
+            $result = $operand1 / $operand2;
+            break;
+        default:
+            die('Неверный оператор: ' . $operator);
+    }
+
+ //   echo $operand1 . ' ' . $operator . ' ' . $operand2 . ' = ' . $result;
+}
+
+include 'form.html';
---
+++ form.tpl.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Калькулятор</title>
+</head>
+<body>
+    <form method="post" action="form.php">
+        <input name="operand1" value="<?= $operand1 ?>">
+        <select name="operator">
+                <option value="+" <?php if ($operator == '+'): ?>selected<?php endif ?>>+</option>
+                <option value="-" <?php if ($operator == '-'): ?>selected<?php endif ?>>-</option>
+                <option value="*" <?php if ($operator == '*'): ?>selected<?php endif ?>>*</option>
+                <option value="/" <?php if ($operator == '/'): ?>selected<?php endif ?>>/</option>
+        </select>
+        <input name="operand2" value="<?= $operand2 ?>">
+        <button type="submit">=</button>
+        <?= $result ?>
+    </form>
+
+
+</body>
+</html>
