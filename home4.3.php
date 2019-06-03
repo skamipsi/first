e039456693575de8868f2fd144d0cfa3713a802d
skamipsi <50922796+skamipsi@users.noreply.github.com>
2019-06-03 17:14:41 +0300

    Update home4.3.php

--- home4.3.php
+++ home4.3.php
@@ -1,15 +1,12 @@
 <?php
-
-
 $dni = [1=>"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 $dayNumber = date('N');
-
 foreach ($dni as $key=>$element){
     if($key == $dayNumber) {
         print "<i>$element</i><br>";
         $day = $element;
     }
+    else {
+        print "$element<br>";
     }
-    print "$element<br>";
-};
-echo  $day;
+}
